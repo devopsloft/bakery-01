fabbf11e210c09167a13f5e9e90f43fc1612c14a
Michael Masas <hot.delek@gmail.com>
2020-11-24 14:01:08 +0200

    echo docker-compose version

--- Michael-Masas/flask_app/docker_setup_centos.sh
+++ Michael-Masas/flask_app/docker_setup_centos.sh
@@ -27,5 +27,6 @@ echo "installing docker compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.27.4/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 
