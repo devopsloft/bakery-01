fa861efa50fef397510cc205c71409033f55db69
root <root@localhost.localdomain>
2020-11-24 23:06:25 -0500

    Added All the files for ex5

---
+++ ex5/api/app.py
@@ -0,0 +1,8 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def welcome():
+     return "Start"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
